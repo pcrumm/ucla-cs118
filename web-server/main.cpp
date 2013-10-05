bb198dc528e369e8d01cbdacfb002914ff5b9b6a
Phillip Crumm <pcrumm@p3net.net>
2013-10-05 14:02:28 -0700

    Let's make it so we can get a port number.

--- web-server/main.cpp
+++ web-server/main.cpp
@@ -10,6 +10,40 @@
  * up and running and hands off the appropriate data for response.
  */
 
-int main() {
-    
+#define DEFAULT_PORT 9529
+#include <unistd.h> // getopt
+#include <iostream> // std::cout, etc.
+
+/**
+ * usage()
+ *
+ * Display the usage message and end execution.
+ */
+void usage() {
+    std::cout << "Simple Webserver for CS118, Fall 2013\n";
+    std::cout << "Created by Phil Crumm and Ivan Petkov.\n";
+    std::cout << "\nUsage:\n" ;
+    std::cout << "./server - Run the server on the default port, " << DEFAULT_PORT << "\n";
+    std::cout << "./server -p X - Run the server on specified port X\n";
+    std::cout << "./server -h - Display this message.\n";
+
+    exit( EXIT_SUCCESS );
+}
+
+int main( int argc, char **argv ) {
+    int port_number = 0;
+
+    for( ;; )
+        switch( getopt( argc, argv, "p:h" ) ) {
+            case 'p': port_number = atoi( optarg ); break;
+            case 'h': default: usage(); break;
+            case -1: goto options_exhausted;
+        }
+    options_exhausted:;
+
+    port_number = port_number != 0 ? port_number : DEFAULT_PORT;
+
+    std::cout << "Starting Simple Web Server on port " << port_number << "\n";
+
+    return EXIT_SUCCESS;
 }
