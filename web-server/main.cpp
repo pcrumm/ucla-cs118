2dcfd97a30b789ac89639a9b362b794afcf864c8
Phillip Crumm <pcrumm@p3net.net>
2013-10-05 13:41:03 -0700

    A stub main.cpp with a simple Makefile

---
+++ web-server/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * Simple Web Server
+ * UCLA, Computer Science 118. Fall 2013.
+ *
+ * @author Phil Crumm pcrumm@ucla.edu
+ * @author Ivan Petkov ipetkov@ucla.edu
+ * @license gpl2
+ *
+ * This is the "entry point" for our application. It makes sure everything is
+ * up and running and hands off the appropriate data for response.
+ */
+
+int main() {
+    
+}
