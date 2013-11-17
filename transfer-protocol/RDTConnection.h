49b55d22f0fb5d4d1c4032778cfd0382d5590214
Ivan Petkov <ivanppetkov@gmail.com>
2013-11-16 16:12:15 -0800

    Initial RDT packet header definition.
    * @pcrumm: Header includes: source and destination ports, sequence and ack
      numbers, the length of the data for the packet, and up to 16 1-bit flags

--- transfer-protocol/RDTConnection.h
+++ transfer-protocol/RDTConnection.h
@@ -7,6 +7,17 @@
 #include <unistd.h>
 #include <string> // std::string
 
+// We want to send one RDT segment per UDP packet and avoid IP fragmentation
+// #define MTU 576 // minimum MTU guaranteed by IPv4
+#define MTU 1500 // ethernet MTU is defined as 1500 bytes
+#define IP_HEADER 20
+#define UDP_HEADER 8
+#define MSS MTU - IP_HEADER - UDP_HEADER // Max payload size for an actual segment
+
+#define ACK_MASK 1 << 2;
+#define SYN_MASK 1 << 1;
+#define FIN_MASK 1 << 0;
+
 class RDTConnection {
 public:
     RDTConnection();
@@ -24,6 +35,30 @@ private:
     int sock_fd;
     sockaddr_in remote_addr;
     sockaddr_in local_addr;
+
+    struct rdt_header_t {
+        uint16_t src_port;
+        uint16_t dst_port;
+        uint16_t seq_num;
+        uint16_t ack_num;
+        uint16_t data_len;
+        uint32_t host_ip; // IP address of host issuing SYN message
+        uint16_t flags;
+    };
+
+    // Do not exceed the max MSS allowed
+    struct rdt_packet_t {
+        rdt_header_t header;
+        char data[ MSS - sizeof(rdt_header_t) ];
+    };
+
+    bool isACK(rdt_packet_t &pkt) { return pkt.header.flags & ACK_MASK; }
+    bool isSYN(rdt_packet_t &pkt) { return pkt.header.flags & SYN_MASK; }
+    bool isFIN(rdt_packet_t &pkt) { return pkt.header.flags & FIN_MASK; }
+
+    void setACK(rdt_packet_t &pkt) { pkt.header.flags |= ACK_MASK; }
+    void setSYN(rdt_packet_t &pkt) { pkt.header.flags |= SYN_MASK; }
+    void setFIN(rdt_packet_t &pkt) { pkt.header.flags |= FIN_MASK; }
 };
 
 #endif
