f5b91fc24f593697fe5daabd0866acdb73b9f802
Ivan Petkov <ivanppetkov@gmail.com>
2013-11-18 08:49:26 -0800

    Adding some untested RDT library function implementations

--- transfer-protocol/RDTConnection.cpp
+++ transfer-protocol/RDTConnection.cpp
@@ -1,4 +1,5 @@
 #include "RDTConnection.h"
+#include <iostream> // std::string
 
 RDTConnection::RDTConnection() : sock_fd( -1 ) {
     memset( &remote_addr, 0, sizeof( remote_addr ));
@@ -9,19 +10,122 @@ RDTConnection::~RDTConnection() {
     close();
 }
 
-bool RDTConnection::connect( std::string afnet_address, int port ) {
-    // TODO
+/**
+ * Creates a socket, binds it, and sends a SYN packet to the remote host
+ * Blocks until an ACK is received or connection times out. Only establishes
+ * a one way connection from local host to the remote host. read_network_packet()
+ * is responsible for responding to any remote SYN requests.
+ *
+ * Returns true if local-to-remote connection established, false otherwise.
+ */
+bool RDTConnection::connect( std::string const &afnet_address, int port ) {
+    close(); // Close existing connection if any
+
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = 0;
+    local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_port = htons(port);
+
+    // Parse the IP address string
+    if ( inet_pton(AF_INET, afnet_address.c_str(), (void *)&remote_addr.sin_addr.s_addr ) != 1 ) {
+        close();
+        return false;
+    }
+
+    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // Bind socket so we can listen to the remote host
+    if (sock_fd == -1 || bind(sock_fd, (struct sockaddr*)&local_addr, sizeof(local_addr)) < 0) {
+        close();
+        return false;
+    }
+
+    // Set the socket timeout (inactivity) and bail if setting the option fails
+    timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = RDT_TIMEOUT_USEC;
+    if (setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0) {
+        close();
+        return false;
+    }
+
+    // Send a SYN packet to the remote host
+    rdt_packet_t pkt;
+    build_network_packet(pkt, "");
+    setSYN(pkt);
+
+    // Bail on transmission errors
+    if ( !broadcast_network_packet(pkt) ) {
+        close();
+        return false;
+    }
+
+    // Wait until remote host SYNACKs our SYN packet
+    // SYN packets sent by the remote host are replied by read_network_packet();
+    timeval start, now;
+    gettimeofday(&start, NULL);
+    int delta_sec = 0;
+    int delta_usec = RDT_TIMEOUT_USEC;
+
+    do {
+        rdt_packet_t recv_pkt;
+        if (read_network_packet(pkt) && isSYNACK(pkt))
+            return true; // Got the SYNACK, return success!
+        else if (errno == EWOULDBLOCK)
+            break; // Socket timeout, bail
+
+        // Timeout for getting an ACK
+        // this is different from a socket timeout as receiving
+        // garbage packets would keep resetting that timer
+        gettimeofday(&now, NULL);
+    } while (
+            now.tv_usec - start.tv_usec < delta_usec
+        &&  now.tv_sec - start.tv_sec < delta_sec
+    );
+
+    // Timed out
+    close();
     return false;
 }
 
 void RDTConnection::close() {
-    if (sock_fd != -1)
+    // TODO: properly tear down connection
+    if (sock_fd != -1) {
         ::close( sock_fd );
+    }
+
+    sock_fd = -1;
+    memset( &remote_addr, 0, sizeof( remote_addr ));
+    memset( &local_addr, 0, sizeof( local_addr ));
 }
 
-void RDTConnection::listen( int port ) {
-    // TODO
-    return;
+/**
+ * Initializes a socket and listens for incoming connections
+ * If object has an existing connection, it will be closed.
+ *
+ * Returns a bool indicating successful port binding
+ */
+bool RDTConnection::listen( int port ) {
+    close(); // Close existing connection if any
+
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    local_addr.sin_port = htons(port);
+
+    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock_fd == -1) {
+        close();
+        return false;
+    }
+
+    if (bind(sock_fd, (struct sockaddr*)&local_addr, sizeof(local_addr)) < 0) {
+        close();
+        return false;
+    }
+
+    return true;
 }
 
 // RDTConnection RDTConnection::accept() {
@@ -37,4 +141,46 @@ bool RDTConnection::receive_data( std::string &data ) {
     // TODO
     data = "";
     return false;
+}
+
+/**
+ * Initializes a network packet with as much specified data as the packet can hold
+ * Returns the amount of data bytes placed into the packet
+ */
+inline int RDTConnection::build_network_packet(rdt_packet_t &pkt, std::string const &data) {
+    memset((void *)&pkt, 0, sizeof(rdt_packet_t));
+
+    pkt.header.magic_num = RDT_MAGIC_NUM;
+    pkt.header.src_port  = ntohs(local_addr.sin_port);
+    pkt.header.dst_port  = ntohs(remote_addr.sin_port);
+    pkt.header.seq_num   = 0;
+    pkt.header.ack_num   = 0;
+    pkt.header.data_len  = std::min(sizeof(pkt.data), data.size());
+    pkt.header.flags     = 0;
+
+    memcpy(&pkt.data, data.c_str(), pkt.header.data_len);
+    return pkt.header.data_len;
+}
+
+/**
+ * Sends a formatted packet to remote_addr.
+ * Returns true if packet broadcasted properly, false otherwise
+ */
+inline bool RDTConnection::broadcast_network_packet(rdt_packet_t const &pkt) {
+    size_t len = std::min(sizeof(rdt_header_t) + pkt.header.data_len, sizeof(rdt_packet_t));
+    return len == sendto(sock_fd, &pkt, len, 0, (struct sockaddr *)&remote_addr, sizeof(remote_addr));
+}
+
+bool RDTConnection::read_network_packet(rdt_packet_t &pkt) {
+    // TODO
+    return false;
+}
+
+/**
+ * Writes to stdout that a packet was dropped for a given reason
+ */
+void RDTConnection::drop_packet(std::string const &reason) {
+    if (reason != "") {
+        std::cout << "Dropped packet: " << reason << std::endl;
+    }
 }
--- transfer-protocol/RDTConnection.h
+++ transfer-protocol/RDTConnection.h
@@ -2,6 +2,7 @@
 #define RDTConn
 
 #include <sys/socket.h> // Socky stuff
+#include <sys/time.h> // gettimeofday
 #include <netinet/in.h> // sockaddr_in
 #include <arpa/inet.h> // htonl, ntohl, etc.
 #include <unistd.h>
@@ -14,18 +15,22 @@
 #define UDP_HEADER 8
 #define MSS MTU - IP_HEADER - UDP_HEADER // Max payload size for an actual segment
 
-#define ACK_MASK 1 << 2;
-#define SYN_MASK 1 << 1;
-#define FIN_MASK 1 << 0;
+#define SYNACK_MASK 1 << 3; // Separate ACK for SYN to avoid confusion from ACK delays
+#define ACK_MASK    1 << 2;
+#define SYN_MASK    1 << 1;
+#define FIN_MASK    1 << 0;
+
+#define RDT_MAGIC_NUM 0xCABBA6E5
+#define RDT_TIMEOUT_USEC 500000 // 500ms
 
 class RDTConnection {
 public:
     RDTConnection();
     virtual ~RDTConnection();
 
-    bool connect( std::string afnet_address, int port = 0 );
+    bool connect( std::string const &afnet_address, int port = 0 );
     void close();
-    void listen( int port );
+    bool listen( int port );
     RDTConnection accept();
 
     bool send_data( std::string const &data );
@@ -37,12 +42,12 @@ private:
     sockaddr_in local_addr;
 
     struct rdt_header_t {
+        uint32_t magic_num; // Used for packet alignment when reading from network
         uint16_t src_port;
         uint16_t dst_port;
         uint16_t seq_num;
         uint16_t ack_num;
         uint16_t data_len;
-        uint32_t host_ip; // IP address of host issuing SYN message
         uint16_t flags;
     };
 
@@ -52,13 +57,20 @@ private:
         char data[ MSS - sizeof(rdt_header_t) ];
     };
 
+    bool isSYNACK(rdt_packet_t &pkt) { return pkt.header.flags & SYNACK_MASK; }
     bool isACK(rdt_packet_t &pkt) { return pkt.header.flags & ACK_MASK; }
     bool isSYN(rdt_packet_t &pkt) { return pkt.header.flags & SYN_MASK; }
     bool isFIN(rdt_packet_t &pkt) { return pkt.header.flags & FIN_MASK; }
 
+    void setSYNACK(rdt_packet_t &pkt) { pkt.header.flags |= SYNACK_MASK; }
     void setACK(rdt_packet_t &pkt) { pkt.header.flags |= ACK_MASK; }
     void setSYN(rdt_packet_t &pkt) { pkt.header.flags |= SYN_MASK; }
     void setFIN(rdt_packet_t &pkt) { pkt.header.flags |= FIN_MASK; }
+
+    int build_network_packet(rdt_packet_t &pkt, std::string const &data);
+    bool broadcast_network_packet(rdt_packet_t const &pkt);
+    bool read_network_packet(rdt_packet_t &pkt);
+    void drop_packet(std::string const &reason);
 };
 
 #endif
