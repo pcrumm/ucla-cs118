0baf852157ee2378ff1fbbfee56e2a0ceac9e3f0
Ivan Petkov <ivanppetkov@gmail.com>
2013-11-19 13:21:47 -0800

    Adding place holder server and client code for RDT testing

--- transfer-protocol/Client.cpp
+++ transfer-protocol/Client.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "RDTConnection.h"
+
+#define DEFAULT_PORT 9529
+
+RDTConnection *conn = NULL;
+
+void sig_handler( int signal ) {
+    printf( "Caught signal %i, exiting\n", signal );
+
+    if(conn) {
+        delete conn;
+        conn = NULL;
+    }
+
+    exit(signal);
+}
 
 int main() {
-    std::cout << "Hello world!\n\t--Client" << std::endl;
+    conn = new RDTConnection();
+    if (!conn->connect("127.0.0.1", DEFAULT_PORT)) {
+        std::cout << "Connection failed, aborting" << std::endl;
+        exit(-1);
+    }
+
+    std::string server_msg;
+    conn->send_data("Hello world! This is the client!");
+    conn->receive_data(server_msg);
+    conn->close();
+
+    std::cout << "Server said: \"" << server_msg << "\"" << std::endl;
     return 0;
 }
--- transfer-protocol/Server.cpp
+++ transfer-protocol/Server.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include "RDTConnection.h"
 
-int main() {
-    std::cout << "Hello world!\n\t--Server" << std::endl;
+#define DEFAULT_PORT 9529
+
+RDTConnection *server = NULL;
+
+void sig_handler( int signal ) {
+    printf( "Caught signal %i, exiting\n", signal );
+
+    if(server) {
+        delete server;
+        server = NULL;
+    }
+
+    exit(signal);
+}
+
+int main( int argc, char **argv ) {
+    signal( SIGHUP, sig_handler );
+    signal( SIGINT, sig_handler );
+    signal( SIGTERM, sig_handler );
+
+    server = new RDTConnection();
+
+    if (!server->listen(DEFAULT_PORT)) {
+        std::cout << "server listen failed, aborting" << std::endl;
+        exit(-1);
+    } else {
+        std::cout << "Listening on port " << server->port_number() << std::endl;
+    }
+
+    std::string remote_msg;
+    while (true) {
+        if (!server->accept())
+            continue;
+
+        server->receive_data(remote_msg);
+        server->send_data("Hello from the server!");
+        std::cout << "Remote host said: \"" << remote_msg << "\"" << std::endl;
+        server->close();
+    }
+
     return 0;
 }
